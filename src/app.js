ce214f8744c786f0ac0bfa299c0172a011dd10e4
shaket2030 <shaketjh123@gmail.com>
2024-07-02 15:24:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 app.get('/demo',(req,res)=>{
-    res.status(200).send('demo1 endpoint working properly');
+    res.status(200).send('demo endpoint working properly');
 
 });
 module.exports=app;
