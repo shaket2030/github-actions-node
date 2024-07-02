5697b4eaea772ba071aefffd70160b6e117950c9
saket <saketjh123@gmail.com>
2024-07-02 15:54:44 +0530

    push github action 5

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@ const request=supertest(app);
 describe('/demo endpoint',()=>{
     it('should return a response',async()=>{
         const response=await request.get('/demo');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('demo endpoint working properly');
+        expect(response.status).toBe(400);
+        expect(response.text).toBe('demo endpoint not working properly');
     });
 });
